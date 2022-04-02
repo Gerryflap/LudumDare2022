15739a2a292481c91a64feca16044e154254077d
gerryflap <gerry2211@gmail.com>
2022-04-02 13:03:59 +0200

    Initial UI commit

---
+++ core/src/nl/lipsum/ui/UiController.java
@@ -0,0 +1,16 @@
+package nl.lipsum.ui;
+
+/**
+ * Controller of the static user interface (minimap and top/bottom bars)
+ */
+public class UiController {
+
+
+    public void render() {
+
+    }
+
+    public void dispose() {
+
+    }
+}
