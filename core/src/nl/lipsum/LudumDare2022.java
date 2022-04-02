e8901f8d380ef832e5bd2fd73106e9f4a40a16d2
gerryflap <gerry2211@gmail.com>
2022-04-02 13:14:52 +0200

    Created framework for controllers

--- core/src/nl/lipsum/LudumDare2022.java
+++ core/src/nl/lipsum/LudumDare2022.java
@@ -1,6 +1,8 @@
 package nl.lipsum;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,18 +10,25 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class LudumDare2022 extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Camera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera(1280, 720);
 	}
 
 	@Override
 	public void render () {
+		// Step Methods called here for controllers
+
+		camera.update();
+
 		ScreenUtils.clear(1, 0, 0, 1);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		// Render methods called here for controllers
 		batch.end();
 	}
 	
