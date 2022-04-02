622617f033817a0172b082f5e8e1f10c96c5adda
MaukWM <Hendrik.nl1>
2022-04-02 22:54:30 +0200

    added death and spawn sound for infantry units

--- core/src/nl/lipsum/entities/EntitySoundType.java
+++ core/src/nl/lipsum/entities/EntitySoundType.java
@@ -2,16 +2,27 @@ package nl.lipsum.entities;
 
 public enum EntitySoundType {
 
-    FIRE("fire.wav"),
-    DEATH("death.wav");
+    FIRE("fire.wav", 0.2f),
+    DEATH("death.wav", 0.7f),
+    SPAWN("spawn.wav", 1f);
 
     String path;
+    float loudness;
 
     EntitySoundType(String path) {
+        this(path, 1);
+    }
+
+    EntitySoundType(String path, float loudness) {
         this.path = path;
+        this.loudness = loudness;
     }
 
     public String getPath() {
         return path;
+    }
+
+    public float getLoudness() {
+        return loudness;
     }
 }
